906d6ca0458ee8c11730c4d8555942a24ca154ec
Dika2408 <141701838+Dika2408@users.noreply.github.com>
2024-03-26 09:10:11 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="row p-3 bg-primary text-white">
+        <div class="col-lg-1">
+        <img src="~/assest/img/LOGO-SMK4.png" alt="logo SMKN4">
+    </div>
+    <div class="col-lg-11">
+    <h1>perpus digital</h1>
+    <address>
+        SMKN 4 TASIKMALAYA
+        Jl.Depok, sukamenak, Purbaratu
+    </address>
+    </div>
+    </div>
+</template>
