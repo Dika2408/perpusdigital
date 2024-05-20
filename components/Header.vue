192c71e639ef174c87e8f0e5738453a5bdff1cac
Dika2408 <141701838+Dika2408@users.noreply.github.com>
2024-05-20 10:58:01 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row p-3 bg-primary text-white">
         <div class="col-lg-1">
-        <img src="~/assest/img/LOGO-SMK4.png" alt="logo SMKN4">
+        <img src="~/assest/img/LOGO-SMK4 (2).png" alt="logo SMKN4">
     </div>
     <div class="col-lg-11">
     <h1>perpus digital</h1>
---
+++ pengunjung/tambah.vue
@@ -0,0 +1,35 @@
+<!-- <template>
+    <div class="content"
+        div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2 class="text-center my-4 text-dark">RIWAYAT KUNJUNGAN</h2>
+                    <div class="my-3">
+                    </div>
+                    <div class="my-3 text-muted">menampilkan 1 dari </div>
+                    <table class="table table-bordered border-dark text-black">
+                        <thead>
+                            <tr>
+                                <td>#</td>
+                                <td>NAMA</td>
+                                <td>KATEGORI</td>
+                                <td>WAKTU</td>
+                                <td>KEPERLUAN</td>
+                            </tr>
+                        </thead>
+                        <tbody>+
+                            <tr>
+                                <td>1.</td>
+                                <td>dikzeu</td>
+                                <td>Siswa</td>
+                                <td>20 mei 2024 23.31.00</td>
+                                <td>Baca</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+                </div>
+                
+
+-->
